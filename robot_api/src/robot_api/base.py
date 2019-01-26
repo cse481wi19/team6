d74ed7912ebc88c59e905fb1d9d3b34c612f5c89
team6 <team6@iff.com>
2019-01-26 14:55:13 -0800

    lab2 done!

--- robot_api/src/robot_api/base.py
+++ robot_api/src/robot_api/base.py
@@ -2,6 +2,7 @@
 
 # TODO: import ????????_msgs.msg
 import rospy
+from  geometry_msgs.msg import Twist
 
 
 class Base(object):
@@ -16,7 +17,7 @@ class Base(object):
 
     def __init__(self):
         # TODO: Create publisher
-        pass
+        self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
 
     def move(self, linear_speed, angular_speed):
         """Moves the base instantaneously at given linear and angular speeds.
@@ -33,10 +34,16 @@ class Base(object):
         # TODO: Create Twist msg
         # TODO: Fill out msg
         # TODO: Publish msg
-        rospy.logerr('Not implemented.')
+        twist = Twist()
+        twist.linear.x = linear_speed
+        twist.angular.z = angular_speed
+        self.pub.publish(twist)
 
     def stop(self):
         """Stops the mobile base from moving.
         """
         # TODO: Publish 0 velocity
-        rospy.logerr('Not implemented.')
+        twist = Twist()
+        twist.linear.x = 0
+        twist.angular.z = 0
+        self.pub.publish(twist)
