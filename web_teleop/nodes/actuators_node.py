54229a54e58f4efbffdd2466999a8efb1c52c867
team6 <team6@iff.com>
2019-01-27 16:13:35 -0800

    Implemented arm and grip

--- web_teleop/nodes/actuators_node.py
+++ web_teleop/nodes/actuators_node.py
@@ -2,8 +2,10 @@
 
 import robot_api
 import rospy
-from web_teleop.srv import SetTorso, SetTorsoResponse, SetHead, SetHeadResponse
-
+from web_teleop.srv import SetTorso, SetTorsoResponse
+from web_teleop.srv import SetHead, SetHeadResponse
+from web_teleop.srv import SetGrip, SetGripResponse
+from web_teleop.srv import SetArm, SetArmResponse
 
 def wait_for_time():
     """Wait for simulated time to begin.
@@ -16,6 +18,8 @@ class ActuatorServer(object):
     def __init__(self):
         self._torso = robot_api.Torso()
         self._head = robot_api.Head()
+        self._grip = robot_api.Gripper()
+        self._arm = robot_api.Arm()
 
     def handle_set_torso(self, request):
         # TODO: move the torso to the requested height
@@ -27,6 +31,17 @@ class ActuatorServer(object):
         self._head.pan_tilt(request.pan, request.tilt)
         return SetHeadResponse()
 
+    def handle_set_grip(self, request):
+        if request.open:
+            self._grip.open()
+        else:
+            self._grip.close()
+        return SetGripResponse()
+
+    def handle_set_arm(self, request):
+        self._arm.move_to_joints(robot_api.ArmJoints.from_list([0, 0, 0, 0, 0, 0, 0]))
+        return SetArmResponse()
+
 
 def main():
     rospy.init_node('web_teleop_actuators')
@@ -34,8 +49,16 @@ def main():
     server = ActuatorServer()
     torso_service = rospy.Service('web_teleop/set_torso', SetTorso,
                                   server.handle_set_torso)
+
     head_service = rospy.Service('web_teleop/set_head', SetHead,
                                   server.handle_set_head)
+
+    arm_service = rospy.Service('web_teleop/set_arm', SetArm,
+                                  server.handle_set_arm)
+
+    grip_service = rospy.Service('web_teleop/set_grip', SetGrip,
+                                  server.handle_set_grip)
+
     rospy.spin()
 
 
