98a7b7a039bcc8ecda96cd500ff0901db8e38f3c
team6 <team6@iff.com>
2019-03-07 13:07:24 -0800

    change object_markers output topic name

--- perception/src/object_detection.cpp
+++ perception/src/object_detection.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh;
 
   // marker publisher for the segmented plane
-  ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("visualization_marker", 1, true);
+  ros::Publisher marker_pub = nh.advertise<visualization_msgs::Marker>("object_markers", 1, true);
 
   perception::ObjectDetector detector(marker_pub);
   ros::Subscriber sub = nh.subscribe("cloud_in", 1, &perception::ObjectDetector::Callback, &detector);
