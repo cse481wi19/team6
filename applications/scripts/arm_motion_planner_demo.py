17d428c3c28dd4cc22e9ccff0ae552836cf9fe9f
team6 <team6@iff.com>
2019-03-09 20:11:13 -0800

    arm motion planner minimum goal

--- applications/scripts/arm_motion_planner_demo.py
+++ applications/scripts/arm_motion_planner_demo.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-
+import copy
 import robot_api
 from robot_api import ArmMotionPlanner
 import gripper_program
@@ -24,12 +24,14 @@ def wait_for_time():
 
 class MarkerReader(object):
     def __init__(self):
-        self.obj_marker = None
+        self.obj_markers = set()
         print("Initialized MarkerReader")
+        # self.obj_marker = None
 
     def callback(self, msg):
-        if msg.type == Marker.CUBE:
-            self.obj_marker = msg
+        if msg.type == Marker.CUBE and msg.pose.position.x < 0.6:
+            self.obj_markers.add(msg)
+            # self.obj_marker = msg
 
 def print_usage():
     print('rosrun application load_exec_program <name>')
@@ -46,21 +48,29 @@ def main():
     # rospy.sleep(0.5)
 
     torso = robot_api.Torso()
-    torso.set_height(robot_api.Torso.MAX_HEIGHT)
-
+    torso.set_height(0)
+    arm = robot_api.Arm()
     gripper = robot_api.Gripper()
-    gripper.open()
+
     reader = MarkerReader()
     marker_sub = rospy.Subscriber('object_markers', Marker, callback=reader.callback)
-    # rate = rospy.Rate(10)
-    # rate.sleep()
+
     rospy.sleep(2)
-    arm = robot_api.Arm()
-    arm_planner = ArmMotionPlanner(arm)
+    arm_planner = ArmMotionPlanner(arm, gripper)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         # print(reader.obj_marker)
-        arm_planner.pick_up(reader.obj_marker)
+        obj_list = copy.deepcopy(reader.obj_markers)
+        # obj_list = set(copy.deepcopy(obs_list))
+        for obj_marker in obj_list:
+            # for obs_marker in obs_list:
+            #     print(obs_marker)
+            # print()
+            # print(obj_marker)
+            # obs_list.remove(obj_marker)
+            arm_planner.pick_up(obj_marker)
+            # obs_list.add(obj_marker)
+        # arm_planner.pick_up(reader.obj_marker)
         rate.sleep()
 
     # rospy.spin()
--- robot_api/src/robot_api/arm_motion_planner.py
+++ robot_api/src/robot_api/arm_motion_planner.py
@@ -13,6 +13,8 @@ import actionlib
 import robot_controllers_msgs
 from robot_controllers_msgs.msg import ControllerState, QueryControllerStatesGoal
 
+PREGRASP = 0.14
+
 def pose_to_transform(pose):
     q = pose.orientation
     matrix = tft.quaternion_matrix([q.x, q.y, q.z, q.w])
@@ -44,50 +46,85 @@ def compute_absolute_pose(obj_ps, goal_tf):
 
 def extract_marker_info(obj_marker):
     obj_ps = PoseStamped()
-    obj_ps.header.frame_id = 'base_link'
+    obj_ps.header.frame_id = obj_marker.header.frame_id
     obj_ps.pose = obj_marker.pose
     obj_scale = obj_marker.scale
     return obj_ps, obj_scale
 
-def try_grasp_plan(obj_ps, obj_scale):
-    pregrasp_in_obj = np.array([
-        [1, 0, 0, obj_scale.y/2 - 0.1],
-        [0, 1, 0,    0],
-        [0, 0, 1,    0],
-        [0, 0, 0,    1],
-    ])
-    if not self._arm.compute_ik(pregrasp_ps):
-        return None
-    return pregrasp_in_obj
+
 
 class ArmMotionPlanner(object):
-    def __init__(self, arm):
+    def __init__(self, arm, gripper):
         self._arm = arm
+        self._gripper = gripper
         print("Initialized ArmMotionPlanner")
         # TODO: have a collision map?
         # self.planning_scene = PlanningSceneInterface(frame='base_link')
 
     # def open(self):
     #     self._gripper.open()
+    def try_grasp_plan(self, obj_ps, obj_scale):
+        z_pos = obj_ps.pose.position.z
+        if obj_scale.y < obj_scale.x:
+            pregrasp_in_obj = np.array([
+                [ 0, 0, 1,    0],
+                [ 0, 1, 0,    0],
+                [-1, 0, 0,     z_pos + obj_scale.z/2 + PREGRASP],
+                [ 0, 0, 0,    1],
+            ])
+        else:
+            pregrasp_in_obj = np.array([
+                [ 0, -1, 0,    0],
+                [ 0,  0, 1,    0],
+                [-1,  0, 0,     z_pos + obj_scale.z/2 + PREGRASP],
+                [ 0,  0, 0,    1],
+            ])
 
+        # print(pregrasp_in_obj)
+        pregrasp_ps = compute_absolute_pose(obj_ps, pregrasp_in_obj)
+        grasp_in_obj = copy.deepcopy(pregrasp_in_obj)
+        grasp_in_obj[2, 3] -= 0.04
+        grasp_ps = compute_absolute_pose(obj_ps, grasp_in_obj)
+        # print(pregrasp_ps)
+        # print(grasp_ps)
+        error = self._arm.check_pose(pregrasp_ps)
+        if error is not None:
+            print("pre_grasp failed")
+            return None, None
+
+        return pregrasp_ps, grasp_ps
 
     def pick_up(self, obj_marker, obs_marker_list=None):
         # TODO: for each obstacle in list, add add_obstacle
-        if obj_marker == None:
+        if obj_marker is None:
             rospy.logerr("Passed in None")
-            return false
+            return False
+
+
+        self.add_obstacles(obs_marker_list)
+
+        self._gripper.open()
+        start = PoseStamped()
+        start.header.frame_id = 'base_link'
+        start.pose.position.x = 0.77
+        start.pose.position.y = -0.1
+        start.pose.position.z = 0.55
+
+        error = self._arm.move_to_pose(start)
+        if error is not None:
+            rospy.logerr(error)
+            print("start position failed.")
+            self.remove_obstacles(obs_marker_list)
+            return False
 
         obj_ps, obj_scale = extract_marker_info(obj_marker)
-        pregrasp_in_obj = try_grasp_plan(obj_ps, obj_scale)
-        if pregrasp_in_obj is None:
-            return false
-        grasp_in_obj = copy.deepcopy(pregrasp_in_obj)
-        grasp_in_obj[0, 3] += 0.1 # x_obj += 0.15
-        lift_in_obj = copy.deepcopy(grasp_in_obj)
-        lift_in_obj[2, 3] += 0.3 # z_obj += 0.3
-        pregrasp_ps = compute_absolute_pose(obj_ps, pregrasp_in_obj)
-        grasp_ps = compute_absolute_pose(obj_ps, grasp_in_obj)
-        lift_ps = compute_absolute_pose(obj_ps, lift_in_obj)
+        pregrasp_ps, grasp_ps = self.try_grasp_plan(obj_ps, obj_scale)
+        if pregrasp_ps is None:
+            print("Not reachable")
+            self.remove_obstacles(obs_marker_list)
+            return False
+
+
         kwargs = {
             'allowed_planning_time': 20,
             'execution_timeout': 20,
@@ -98,28 +135,62 @@ class ArmMotionPlanner(object):
         error = self._arm.move_to_pose(pregrasp_ps, **kwargs)
         if error is not None:
             rospy.logerr(error)
-            return false
+            self.remove_obstacles(obs_marker_list)
+            return False
 
-        return true
+        error = self._arm.check_pose(grasp_ps)
+        if error is not None:
+            print("grasp failed")
+            self.remove_obstacles(obs_marker_list)
+            return False
+
+        error = self._arm.move_to_pose(grasp_ps, **kwargs)
+        if error is not None:
+            rospy.logerr(error)
+            self.remove_obstacles(obs_marker_list)
+            return False
+
+        self._gripper.close()
+        error = self._arm.move_to_pose(start)
+        if error is not None:
+            rospy.logerr(error)
+            print("lift position failed.")
+            self.remove_obstacles(obs_marker_list)
+            return False
+
+        self.remove_obstacles(obs_marker_list)
+        return True
         # TODO: Need to find a suitable pre_grasp position
         # TODO: close gripper after grasph_ps
         # TODO: lift_ps should be visible
         # TODO2: when lifting, avoid twist of arm???
         # TODO: remove all_obstacles.
 
-    def add_obstacle(self, obs_ps):
-        self.planning_scene.removeCollisionObject('table')
-        # Create table obstacle
-        table_size_x = 0.5
-        table_size_y = 1
-        table_size_z = 0.03
-        table_x = 0.8
-        table_y = 0
-        table_z = 0.6
-        self.planning_scene.addBox('table', table_size_x, table_size_y, table_size_z,
-                              table_x, table_y, table_z)
+    def add_obstacles(self, obs_marker_list):
+        if obs_marker_list is None:
+            return
+        i = 0
+        for obs_marker in obs_marker_list:
+            obs_ps, obs_scale = extract_marker_info(obs_marker)
+            obs_size_x = obs_scale.x
+            obs_size_y = obs_scale.y
+            obs_size_z = obs_scale.z
+            obs_x = obs_ps.pose.position.x
+            obs_y = obs_ps.pose.position.x
+            obs_z = obs_ps.pose.position.x
+            self.planning_scene.addBox('obs' + i, obs_size_x, obs_size_y, obs_size_z,
+                                  obs_x, obs_y, obs_z)
+            i += 1
         return
 
+    def remove_obstacles(self, obs_marker_list):
+        if obs_marker_list is None:
+            return
+        i = 0
+        for obs_ps in obs_ps_list:
+            self.planning_scene.removeCollisionObject('obs' + i)
+            i += 1
+        return
 
 # def shutdown():
 #     arm.cancel_all_goals()
